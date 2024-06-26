2aad663ba86ebdb8b99c70b7179d68775c9d6a30
LaughingMaan <lawilas805@yandex.ru>
2024-06-26 23:11:19 +0300

    Update Main.o.d

--- build/default/production/Main.o.d
+++ build/default/production/Main.o.d
@@ -1,6 +1,2 @@
-build/default/production/Main.o:  \
-	C:/Users/Andrey/Desktop/DATA/MPLABXProjects/AlexeyPolozov.X/Main.asm  \
-	C:/Users/Andrey/Desktop/DATA/MPLABXProjects/AlexeyPolozov.X/Init.inc  \
-	C:/Users/Andrey/Desktop/DATA/MPLABXProjects/AlexeyPolozov.X/Memory.inc  \
-	C:/Users/Andrey/Desktop/DATA/MPLABXProjects/AlexeyPolozov.X/Interrupt.inc  \
+
 
